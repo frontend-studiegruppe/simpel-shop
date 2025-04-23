138e03f9bb4149ffa4e198a7541f1b73ab8ca816
MajaOlsen <majabacholsen@gmail.com>
2025-04-23 16:25:21 +0200

    footer styling done

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Card from "@/components/Card";
 import { ReviewBar } from "@/components/singleview/Reviews";
+import Footer from "@/components/Footer";
 
 export default function Home() {
   return (
@@ -9,6 +10,7 @@ export default function Home() {
       <p>Eksempel på brødtekst</p>
       <Card></Card>
       <ReviewBar></ReviewBar>
+      <Footer />
     </div>
   );
 }
