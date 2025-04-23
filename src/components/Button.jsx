ac0723b2e7196915e2e3f118fcd32643bcc20cb4
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-04-23 17:36:22 +0200

    update button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -8,7 +8,6 @@ const Button = ({ children, variant }) => {
       "py-4 px-8 bg-[var(--color-secondary-cherry-light)] text-white text-xl hover:bg-white hover:text-[var(--color-secondary-cherry-light)] hover:border-2 hover:border-[var(--color-secondary-cherry-light)]",
     secondary:
       "px-10 text-m bg-white text-[var(--color-secondary-cherry-light)] border-2 border-[var(--color-secondary-cherry-light)] hover:bg-[var(--color-secondary-cherry-light)] hover:text-white",
-    tertiary: "",
   };
   return (
     <div className={`w-fit  shrink-0 cursor-pointer ${variants[variant]}`}>
