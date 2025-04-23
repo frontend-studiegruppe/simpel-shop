0335850ed8ba80dbdee8bb5f7f52c60f63360ca9
MajaOlsen <majabacholsen@gmail.com>
2025-04-23 22:29:57 +0200

    sizedropdown

--- src/app/page.js
+++ src/app/page.js
@@ -4,18 +4,19 @@ import ReviewCard, { ReviewBar } from "@/components/singleview/Reviews";
 import Footer from "@/components/Footer";
 import Button from "@/components/Button";
 import Link from "next/link";
+import SizeDropdown from "@/components/singleview/SizeDropdown";
 
 export default function Home() {
   return (
     <div>
-      <h1 className="flex justify-center text-secondary-cherry-dark text-8xl font-bold ">
-        Studio Rouge
-      </h1>
+      <h1 className="flex justify-center text-secondary-cherry-dark text-8xl font-bold ">Studio Rouge</h1>
       <div className="flex justify-center">
         <Link href="/products">
           <Button variant="primary">See products</Button>
         </Link>
       </div>
+      <SizeDropdown size="s" />
+      <SizeDropdown size="m" />
 
       <p>Eksempel på brødtekst</p>
       <Card></Card>
---
+++ src/components/singleview/SizeDropdown.jsx
@@ -0,0 +1,44 @@
+"use client";
+import { useState } from "react";
+import { IoIosArrowDown } from "react-icons/io";
+
+const SizeDropdown = ({ size = "m" }) => {
+  const [selected, setSelected] = useState("m");
+  const [isOpen, setIsOpen] = useState(false);
+  const sizes = ["s", "m", "l", "xl", "xxl"];
+
+  const sizeStyles = {
+    s: "w-24 text-sm px-2 py-1",
+    m: "w-40 text-base px-4 py-2",
+  }; //forskellige størrelser til payment og singleview (omfg har lige indset det ikek er til sizes men til quantity im just gonna scream)
+
+  return (
+    <div className="inline-block relative">
+      <label className="text-sm mb-1 block">Choose size:</label>
+
+      <div className={`bg-gray-300 flex justify-between items-center cursor-pointer ${sizeStyles[size]}`} onClick={() => setIsOpen(!isOpen)}>
+        <span>{selected}</span>
+        <IoIosArrowDown />
+      </div>
+
+      {isOpen && (
+        <div className={`absolute bg-gray-300  z-10 ${sizeStyles[size]}`}>
+          {sizes.map((sizeOption) => (
+            <div
+              key={sizeOption}
+              className="px-4 py-2 hover:bg-gray-400 cursor-pointer"
+              onClick={() => {
+                setSelected(sizeOption);
+                setIsOpen(false);
+              }}
+            >
+              {sizeOption}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default SizeDropdown;
