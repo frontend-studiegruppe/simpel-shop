a31351df7d8ec53438e81efa936781aa2466a5ac
matilde <mabr0005@stud.kea.dk>
2025-04-23 10:17:38 +0200

    import af font og farver

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,10 @@
 import Image from "next/image";
 
 export default function Home() {
-  return <div>Simpel Shop</div>;
+  return (
+    <div>
+      <h1 className="text-secondary-cherry-dark text-3xl">Simpel Shop</h1>
+      <p>Eksempel på brødtekst</p>
+    </div>
+  );
 }
