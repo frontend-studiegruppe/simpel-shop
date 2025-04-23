0b9bd4ee8a014f938bbdd1b50dee22004f561615
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-04-23 17:43:44 +0200

    bla

--- src/components/singleview/Reviews.jsx
+++ src/components/singleview/Reviews.jsx
@@ -1,23 +1,25 @@
 import { GrStar, GrStarOutline } from "react-icons/gr";
 
 const ReviewCard = () => {
-    return ( <div></div> );
-}
+  return <div></div>;
+};
 
 const ReviewBar = () => {
-    return ( <div className="flex text-2xl">
-        <GrStar />
-        <GrStar />
-        <GrStar />
-        <GrStarOutline />
-        <GrStarOutline />
-    </div> );
-}
+  return (
+    <div className="flex text-2xl">
+      <GrStar />
+      <GrStar />
+      <GrStar />
+      <GrStarOutline />
+      <GrStarOutline />
+    </div>
+  );
+};
 
 const ReviewStar = () => {
-    return ( <div></div> );
-}
- 
+  return <div></div>;
+};
+
 export default ReviewCard;
 export { ReviewBar };
 export { ReviewStar };
