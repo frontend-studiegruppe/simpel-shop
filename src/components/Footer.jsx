e1700e72d7ee394ed2b15be5c62f1d9c7914fd91
matilde <mabr0005@stud.kea.dk>
2025-04-23 12:29:00 +0200

    icons header og footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,1 @@
+import { BiLogoLinkedin, BiLogoInstagram, BiLogoFacebook } from "react-icons/bi";
---
+++ src/components/Header.jsx
@@ -0,0 +1,2 @@
+import { PiHandbagLight } from "react-icons/pi";
+import { IoIosSearch } from "react-icons/io";
