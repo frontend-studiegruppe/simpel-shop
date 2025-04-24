3ff9956ca9930ba95ed0a34b544b929138d6030d
matilde <mabr0005@stud.kea.dk>
2025-04-24 12:15:27 +0200

    button fixes

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -1,11 +1,12 @@
 //Katinka: OBS mangler icon på button
 
 import Button from "@/components/Button";
+import { PiCreditCardLight } from "react-icons/pi";
 
 const Payment = () => {
   return (
     <div>
-      <Button variant="primary">Buy now</Button>
+      <Button variant="primary">Buy now <PiCreditCardLight size={34} className="ml-4"/> </Button>
     </div>
   );
 };
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -5,12 +5,12 @@
 const Button = ({ children, variant }) => {
   const variants = {
     primary:
-      "py-4 px-8 bg-[var(--color-secondary-cherry-light)] text-white text-xl hover:bg-white hover:text-[var(--color-secondary-cherry-light)] hover:border-2 hover:border-[var(--color-secondary-cherry-light)]",
+      "py-4 px-8 bg-[var(--color-secondary-cherry-light)] text-white text-xl border-2 border-secondary-cherry-light border- hover:bg-white hover:text-[var(--color-secondary-cherry-light)] hover:border-2 hover:border-[var(--color-secondary-cherry-light)]",
     secondary:
       "px-10 text-m bg-white text-[var(--color-secondary-cherry-light)] border-2 border-[var(--color-secondary-cherry-light)] hover:bg-[var(--color-secondary-cherry-light)] hover:text-white",
   };
   return (
-    <div className={`w-fit  shrink-0 cursor-pointer ${variants[variant]}`}>
+    <div className={`w-fit flex items-center shrink-0 cursor-pointer ${variants[variant]}`}>
       {children}
     </div>
   );
