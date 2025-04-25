5298609c5687089c4293158a118c17c6923b8d6d
matilde <mabr0005@stud.kea.dk>
2025-04-25 10:24:45 +0200

    sale ikon på card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -9,10 +9,14 @@ const Card = ({ product }) => {
   }
 
   return (
-    <Link href={`products/${product.id}`}>
+    <Link href={`/products/${product.id}`}>
     <div className="group max-w-[250px]">
       <div className="relative">
-        <Image src={product.thumbnail || "/placeholder.png"} alt={product.title} width={250} height={250} className="group-hover:brightness-50 transition duration-300 ease-in-out object-cover bg-white" />
+        {product.discountPercentage > 10 && (<div className="bg-secondary-cherry-light/30 text-secondary-cherry-dark font-bold w-fit px-2 py-1 absolute top-0 left-0 z-10">
+        <h3>Sale {Math.round(product.discountPercentage)}%</h3>
+        </div>)}
+        
+        <Image src={product.thumbnail || "/placeholder.png"} alt={product.title} width={250} height={250} className="group-hover:brightness-50 transition duration-300 ease-in-out object-cover bg-white z-9" />
         <div className="w-full flex justify-center absolute bottom-4 left-1/2 -translate-x-1/2 opacity-0 group-hover:opacity-100 transition duration-300 ease-in-out">
           <Button variant="card_hover_btn">Add to Basket</Button>
         </div>
