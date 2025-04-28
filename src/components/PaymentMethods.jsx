a6de9cd28a03908876f534f4d65f956954d2943b
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-04-28 10:06:37 +0200

    update

--- src/components/PaymentMethods.jsx
+++ src/components/PaymentMethods.jsx
@@ -1,3 +1,19 @@
+import { FaCcVisa } from "react-icons/fa";
+import { FaCcMastercard } from "react-icons/fa";
+import { SiKlarna } from "react-icons/si";
+import { FaCcPaypal } from "react-icons/fa";
+import { MdCardGiftcard } from "react-icons/md";
 
+const PaymentMethods = () => {
+  return (
+    <div className="flex justify-between">
+      <FaCcVisa size={34} />
+      <FaCcMastercard size={34} />
+      <SiKlarna size={34} />
+      <FaCcPaypal size={34} />
+      <MdCardGiftcard size={34} />
+    </div>
+  );
+};
 
-
+export default PaymentMethods;
