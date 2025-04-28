7bb66ee98ec1963796c518febc61d4334cc02a85
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-04-28 10:18:23 +0200

    update color boxes

--- src/components/singleview/ColorSelector.jsx
+++ src/components/singleview/ColorSelector.jsx
@@ -1,9 +1,11 @@
 const ColorSelector = () => {
-    return ( <div className="flex gap-4">
-        <div className="h-10 w-10 bg-gray-500 border-1 border-black"></div>
-        <div className="h-10 w-10 bg-gray-500 border-1 border-black"></div>
-        <div className="h-10 w-10 bg-gray-500 border-1 border-black"></div>
-    </div> );
-}
- 
+  return (
+    <div className="flex gap-4">
+      <div className="h-10 w-10 bg-[#98DAA0] border-1 border-black"></div>
+      <div className="h-10 w-10 bg-[#D0F5FF] border-1 border-black"></div>
+      <div className="h-10 w-10 bg-[#FFD4D4] border-1 border-black"></div>
+    </div>
+  );
+};
+
 export default ColorSelector;
