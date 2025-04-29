6d548bbce3f4d2ce22168bd73a49b3ae1212888a
Katinka H. Bjerke <kabj0002@stud.kea.dk>
2025-04-29 11:03:33 +0200

    update card item responsivitet

--- src/app/payment/page.jsx
+++ src/app/payment/page.jsx
@@ -14,26 +14,28 @@ const PaymentPage = () => {
   const { cart, clearCart, cartQuantity } = useCartStore();
   return (
     <div className="grid lg:grid-cols-[2fr_1fr] sm:grid-cols-1 gap-4 mb-16">
-      <div className="bg-primary-grey-light-2 grid gap-6 px-24 py-16 h-fit">
+      <div className="bg-primary-grey-light-2 grid gap-6 px-10 sm:px-24 lg:px-10 py-16 h-fit">
         <h2 className="text-secondary-cherry-dark font-bold text-2xl">Cart</h2>
         <div className="flex flex-col gap-4 justify-start">
-        {cart.length === 0 ? (
-        <div>
-          <p>Your cart is empty</p>
-          
-        </div>
-      ) :      <>
-      {cart.map((product) => (
-        <CartItem key={product.id} product={product} />
-      ))}
-            <p>Items in cart: {cartQuantity}</p>
-      <Button variant="primary" onClick={clearCart}>Clear Cart</Button>
-    </>
-    }
+          {cart.length === 0 ? (
+            <div>
+              <p>Your cart is empty</p>
+            </div>
+          ) : (
+            <>
+              {cart.map((product) => (
+                <CartItem key={product.id} product={product} />
+              ))}
+              <p>Items in cart: {cartQuantity}</p>
+              <Button variant="primary" onClick={clearCart}>
+                Clear Cart
+              </Button>
+            </>
+          )}
         </div>
       </div>
 
-      <div className="bg-primary-grey-light-2 py-16 px-14 h-fit">
+      <div className="bg-primary-grey-light-2 py-16 px-10 sm:px-24 lg:px-10 h-fit">
         <h2 className="text-secondary-cherry-dark font-bold mb-6 text-2xl">
           Payment
         </h2>
